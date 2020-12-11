5fef2ec9be9b4725f0f36c6f6c63ecbe8a7f1a49
I20050041 <71293482+I20050041@users.noreply.github.com>
2020-12-11 13:16:02 -0600

    Add files via upload

---
+++ conectar/conectar/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace conectar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c;
+            a = 16;
+            b = 12;
+            c = a + b;
+            Console.Write("el resultado es: " + c);
+            Console.ReadKey();
+
+
+        }
+    }
+}
